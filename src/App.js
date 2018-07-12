364968df595de4ad05ebacda07966482827eb180
NeuroN1nja <38508227+NeuroN1nja@users.noreply.github.com>
2018-07-12 19:30:15 +0500

    Update App.js
    fix legacy

--- src/App.js
+++ src/App.js
@@ -4,52 +4,48 @@ import List from './components/List'
 
 
 class App extends Component {
-  constructor(props) {  
-    super(props);
+  state = { 
+    tasks: []
+  }
 
-    this.state = { 
-      tasks: []
+
+  nextId = () => {
+    this._nextId = this.state.tasks.length;
+    return this._nextId += 1;
+  }
+
+  addTask = (text) => {
+    const newtask = {
+      id: this.nextId(),
+      text: text
     }
-    
-  this._nextId = this.state.tasks.length;
-}
-
-nextId() {
-  return this._nextId += 1;
-}
-
-addTask = (text) => {
-  const newtask = {
-    id: this.nextId(),
-    text: text
+    const tasks = [...this.state.tasks, newtask]
+      this.setState({
+          tasks
+      })
   }
-  const tasks = [...this.state.tasks, newtask]
-    this.setState({
-        tasks
-    })
-}
 
-deleteCard = (id) => {
-  const index = this.state.tasks.findIndex(task => task.id === id);
-  const tasks = [
-      ...this.state.tasks.slice(0, index),
-      ...this.state.tasks.slice(index + 1)
-  ];
-  this.setState({ tasks });
-}
-  render() {
-    return (
-      <main>
-      <Input 
-        addTask={this.addTask}  
-      />
-      <List
-        tasks={this.state.tasks}
-        deleteCard={this.deleteCard}
-       />
-       </main>
-    );
+  deleteCard = (id) => {
+    const index = this.state.tasks.findIndex(task => task.id === id);
+    const tasks = [
+        ...this.state.tasks.slice(0, index),
+        ...this.state.tasks.slice(index + 1)
+    ];
+    this.setState({ tasks });
   }
+    render() {
+      return (
+        <main>
+        <Input 
+          addTask={this.addTask}  
+        />
+        <List
+          tasks={this.state.tasks}
+          deleteCard={this.deleteCard}
+         />
+         </main>
+      );
+    }
 }
 
 export default App;
